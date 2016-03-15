f6be4a2be7b95f91599d843a5719aa4aab85ef96
Hannes <hannesbirgersson@gmail.com>
2016-03-15 14:17:32 +0100

    går att trycka för att få fram närmsta stationer

--- map.js
+++ map.js
@@ -1,21 +1,54 @@
+
+var map;
+var infowindow;
+
 function initMap() {
-    var myLatlng = {lat: 55.605099, lng: 13.003036};
-    
-    var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatlng,
-    zoom: 10,
-    styles: [{
-      featureType: 'poi',
-      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
-    }, {
-      featureType: 'transit.station',
-      stylers: [{ visibility: 'on' }]  // Turn off bus stations, train stations, etc.
-    }],
-    disableDoubleClickZoom: true
+  var malmo = {lat: 55.605099, lng: 13.003036};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: malmo,
+    zoom: 15
   });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  
     
-    marker.addListener('click', function() {
-        map.setZoom(8);
-        map.setCenter(marker.getPosition());
+    
+    map.addListener('click', function(event) {
+        var lat = event.latLng.lat();
+        
+        var lng = event.latLng.lng();
+        
+        var current = {lat: lat, lng: lng};
+        console.log(current);
+        service.nearbySearch({
+            location: current,
+            radius: 500,
+            type: ['bus_station']
+          }, callback);
+        
     });
+}
+
+function callback(results, status) {
+  console.log("kek");
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
