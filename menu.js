e4b354ee6719f05da2edb16da496e1093ccb7467
antaout <anttiautti@hotmail.com>
2016-03-17 19:26:09 +0100

    Menu, visual representation
    Menu, fixed map location when menu is shown.

--- menu.js
+++ menu.js
@@ -1,19 +1,4 @@
 /*Menu functionality*/
-/*
-var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70,
-    'side': 'left'
-  });
-
-// Toggle button
-    document.querySelector('.toggle-button').addEventListener('click', function() {
-    slideout.toggle();
-
-});
-*/
 $(document).ready(function() {
-  $('#simple-menu').sidr();
+  $('#simple-menu-button').sidr();
 });
