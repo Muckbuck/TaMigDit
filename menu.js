71cf95d7ef9c5568cdd0b0eaa4b0b04ce680694b
antaout <anttiautti@hotmail.com>
2016-03-18 18:00:43 +0100

    Menu DateTimeLatest

--- menu.js
+++ menu.js
@@ -28,21 +28,3 @@ $("#simple-menu-button").click(function() {
         
     }
 });
-        
-/*DateTime picker*/
-$('#datetimepicker3').datetimepicker({
-  format:'d.m.Y H:i',
-  inline:true,
-  lang:'sv',
-allowTimes:[
-  '12:00', '13:00', '15:00', 
-  '17:00', '17:05', '17:20', '19:00', '20:00'
- ]
-});
-
-$('#input').datetimepicker();
-$('toggle-time-button').on('click', function () {
-    $('#input').datetimepicker('toggle');
-});
-
-$.datetimepicker.setLocale('sv');
