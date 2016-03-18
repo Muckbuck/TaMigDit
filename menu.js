9a72aa59d86225520f2b36d67ade5a2e0078aa65
antaout <anttiautti@hotmail.com>
2016-03-18 14:20:38 +0100

    CSS for Sidr, Search

--- menu.js
+++ menu.js
@@ -1,4 +1,12 @@
 /*Menu functionality*/
-$(document).ready(function() {
-  $('#simple-menu-button').sidr();
+$(document).ready(function () {
+    $('#simple-menu-button').sidr();
+});
+
+/*DateTime picker*/
+$(function () {
+    $('#datetimepicker12').datetimepicker({
+        inline: true
+        , sideBySide: true
+    });
 });
