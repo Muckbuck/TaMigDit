f01db6d7bde00d026b080ffaf61b86544d1b0911
Hannes <hannesbirgersson@gmail.com>
2016-03-18 14:53:18 +0100

    fixade merg med menyn

--- menu.js
+++ menu.js
@@ -10,20 +10,26 @@ var faded = false;
 $( "#simple-menu-button" ).click(function() {
   
     if (!faded) {
-        $( "#fadebar" ).fadeOut( "slow", function() {
+        faded = true;
+        $( "#destination-field" ).fadeOut( "fast", function() {
             // Animation complete.
         });
+        $( "#go-button" ).fadeOut( "fast", function() {
+            // Animation complete.
+        });
+        
     } else {
-         $( "#fadebar" ).fadeIn( "slow", function() {
+        faded = false;
+        $( "#destination-field" ).fadeIn( "fast", function() {
+            // Animation complete.
+        });
+        $( "#go-button" ).fadeIn( "fast", function() {
             // Animation complete.
         });
         
     }
-        
-$(document).ready(function () {
-    $('#simple-menu-button').sidr();
 });
-
+        
 /*DateTime picker*/
 $(function () {
     $('#datetimepicker12').datetimepicker({
